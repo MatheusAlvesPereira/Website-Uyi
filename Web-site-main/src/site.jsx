908ca93541bd73fbb99a59aa2ddc19b30f2bfa96
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-03-17 19:53:03 -0300

    components

--- Web-site-main/src/site.jsx
+++ Web-site-main/src/site.jsx
@@ -1,13 +1,13 @@
 import './Styles.css'
-import Nav from './Banner/nav.jsx' 
-import Banner from './Banner/Banner.jsx'
-import Section1 from "./Section1/section1.jsx"
-import Section2 from "./Section2/section2.jsx"
-import Section3 from './Section3/Section3.jsx'
-import Carousel from "./Carousel/carousel.jsx"
-import Projetos from "./Projetos/projetos.jsx"
-import Footer from "./footer/footer.jsx"
-import About from "./Section4/about.jsx"
+import Nav from "./components/Banner/nav.jsx"
+import Banner from "./components/Banner/Banner.jsx"
+import Section1 from "./components/Section1/section1.jsx"
+import Section2 from "./components/Section2/section2.jsx"
+import Section3 from "./components/Section3/Section3.jsx"
+import Carousel from "./components/Carousel/carousel.jsx"
+import Projetos from "./components/Projetos/projetos.jsx"
+import Footer from "./components/footer/footer.jsx"
+import About from "./components/Section4/about.jsx"
 
 export default function Site(){
     return<>
