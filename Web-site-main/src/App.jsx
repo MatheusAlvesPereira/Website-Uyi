3e61f657261c38fc3587f19091ca863efee06a56
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-03-21 17:57:20 -0300

    auth login

--- Web-site-main/src/App.jsx
+++ Web-site-main/src/App.jsx
@@ -10,25 +10,12 @@ import RecuperarSenha from "./RecuprarSenha/RecuprarSenha.jsx"
 import Register from "./Register/register.jsx";
 import Site from "./site.jsx"
 import Profile from "./profile/profile.jsx"
-import { AuthContext } from "./Context/Auth.jsx";
-import { useState } from "react";
+import {AuthProvider} from "./Context/Auth.jsx"
 
-const App = () => {
-
-    const [user, setUser] = useState(null);
-    
-    const login = (email,password) => {
-        console.log("login auth", {email, password});
-        setUser({ id:"123", email});
-    };
-
-    const logout = () => {
-        console.log("logout");
-    };
-
+const App = () => {    
     return(
     <Router>
-        <AuthContext.Provider value={{authenticated: !!user, user, login, logout}}>
+        <AuthProvider>
             <Routes>
                 <Route exact path='/' element={<Site/>}/>
                 <Route exact path='/login' element={<Login/>}/>
@@ -36,7 +23,7 @@ const App = () => {
                 <Route exact path='/RecuperarSenha' element={<RecuperarSenha/>}/>
                 <Route exact path='/login/Profile' element={<Profile/>}/>            
             </Routes>
-        </AuthContext.Provider>    
+        </AuthProvider>    
     </Router>
     )
 }
--- Web-site-main/src/Context/Auth.jsx
+++ Web-site-main/src/Context/Auth.jsx
@@ -1,11 +1,37 @@
-import react, {createContext} from "react";
+import react, { useState, createContext} from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export const AuthContext = createContext();
 
-export const AuthProvider = () => {
+export const AuthProvider = ({children}) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+        
+    const login = (email,password) => { 
+        console.log("login auth", {email, password});
+
+        if (password === "secret") {
+            setUser({ id:"123", email});
+            navigate('/login/Profile');
+        }
+
+        else{
+            {/* senão colocar um messagem de usuario ou senha incorretos */}
+        }
+    };
+
+    const logout = () => {
+        console.log("logout");
+        setUser(null);
+        navigate("/login")
+    };
+
+
     return(
         <AuthContext.Provider value={{authenticated: !!user, user, login , logout}}>
-            
+            {children}
         </AuthContext.Provider>
-    )
-}
+    ); 
+        
+};
