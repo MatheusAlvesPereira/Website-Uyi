9f6e87c1cb787f74aa0be8e66c3eafc75898e935
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-04-08 17:24:36 -0300

    need add firebird PDO

---
+++ Web-site-main/src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $username = '#';
+    $password = '#';
+    $dsn = '#';
+    $dbh = null;
+
+    try {
+        $dbh = new PDO($dsn, $username, $password);
+    } 
+    catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+        exit;
+    }
+
+?>
