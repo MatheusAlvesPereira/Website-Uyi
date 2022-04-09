8089f840afd4efac8027caf55e739d4c0d72e9d0
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-04-09 17:31:59 -0300

    add form action, php form

--- Web-site-main/src/Login/Login.jsx
+++ Web-site-main/src/Login/Login.jsx
@@ -1,38 +1,19 @@
 import img1 from "./img1.png"
 import "./login.css"
 import {Link} from 'react-router-dom'
-import React, {useState, useContext} from 'react';
-import { AuthContext } from "../Context/Auth";
 
 const Login = () => {
-
-    const {authenticated , login} = useContext(AuthContext);
-
-    const [email, setEmail] = useState(""); 
-    const [password, setPassword] = useState("");
-
-    const handleSubimit = (event) => {
-        event.preventDefault();
-
-        console.log("submit", {email,password});
-
-        login(email,password);
-    };
-
     return(
         <div className="d-flex align-itens-center text-center form-container">
-            <form className="form-signin" onSubmit={handleSubimit}>
+            <form className="form-signin">
                 <img src={img1} alt="" />
                 <h1 className="h3 mb-3 fw-normal">Login</h1>
-                <p>{String(authenticated)}</p>
 
                 <div className="form-floating">
                     <input 
                      type="email"
                      className="form-control" 
                      id="floatingInput"
-                     value={email}
-                     onChange={(event)=> setEmail(event.target.value)}
                      />
                     <label>E-mail</label>
                 </div>
@@ -42,8 +23,6 @@ const Login = () => {
                      type="password" 
                      className="form-control" 
                      id="floatingPassword"
-                     value={password} 
-                     onChange={(event) => setPassword(event.target.value)} 
                     />
                     <label>Senha</label>
                 </div>
