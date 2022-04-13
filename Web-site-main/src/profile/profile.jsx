ac44a8b92ddbdec8d79e81bbacbfb1861612f39b
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-04-13 15:09:58 -0300

    add myplans to profile

--- Web-site-main/src/profile/profile.jsx
+++ Web-site-main/src/profile/profile.jsx
@@ -1,14 +1,8 @@
-import react, { useContext } from 'react'
-import { AuthContext } from '../Context/Auth';
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Section2 from "../Components/Section2/section2";
+import "./profile.css";
 
 export default function Profile(){
-
-    const {authenticated, logout } = useContext(AuthContext);
-
-    const handleLogout = () => {
-        logout();
-    };
  
     return<>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
@@ -20,16 +14,42 @@ export default function Profile(){
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                 <li className="nav-item">
-                    <a className="nav-link" href="#Section2">Planos</a>
+                    <a className="nav-link" href="#myplans">Planos</a>
                 </li>
                 <li className="nav-item">
-                    <a onClick={handleLogout} href="/login" className='nav-link'>Sair</a>
+                    <a href="/login" className='nav-link'>Sair</a>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className='nav-link'>{String(authenticated)}</a>
+                    <a href="#" className='nav-link'></a>
                 </li>
                 </ul>
             </div>
         </nav>
+
+        <Section2/>
+
+        <div className="myplans_container">
+            <div className="myplans_box">
+            <h2 className="text-plans" id="myplans">Meus Planos</h2>
+                <div className="card_myplans">
+                    <div className="card-header py-3 text-white bg-primary border-primary">
+                        <h4 className="my-0 fw-normal">Gratuito</h4>
+                    </div>
+
+                    <div className="card-body-plans" id="myplan">
+                        <h1 className="card-title pricing-card-title">R$0<small className="text-muted fw-light">/mês</small></h1>
+                            <ul className="list-unstyled mt-3 mb-4">
+                                <li>Acesso a Plataforma.</li>
+                                <li>bootcamps.</li>
+                                <li><s>até 5 sites</s></li>
+                                <li><s>5 Web Designers</s></li>
+                                <li><s>5 Programadores</s></li>
+                                <li><s>2 Analistas</s></li>
+                            </ul>
+                        <button className="w-100 btn btn-lg btn-primary">Assinado</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
