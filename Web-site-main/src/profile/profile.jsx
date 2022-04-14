b98ee080c5980f0c9d5ea6356775cecf413b68f3
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-04-14 10:07:58 -0300

    myplans and responsive large screen

--- Web-site-main/src/profile/profile.jsx
+++ Web-site-main/src/profile/profile.jsx
@@ -1,9 +1,9 @@
 import logo from "./logo.png";
 import Section2 from "../Components/Section2/section2";
 import "./profile.css";
+import imgprofile from "./img1.png";
 
 export default function Profile(){
- 
     return<>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <a className="navbar-brand" href="#"><img src={logo}/></a>
@@ -26,11 +26,18 @@ export default function Profile(){
             </div>
         </nav>
 
+        <div className="myprofile_container">
+            <div className="img_profile_container">
+                <img src={imgprofile} className="img_profile" alt=""/>
+                <div className="profile_name">My name</div>
+            </div>
+        </div>
+
         <Section2/>
 
         <div className="myplans_container">
-            <div className="myplans_box">
             <h2 className="text-plans" id="myplans">Meus Planos</h2>
+            <div className="myplans_box">
                 <div className="card_myplans">
                     <div className="card-header py-3 text-white bg-primary border-primary">
                         <h4 className="my-0 fw-normal">Gratuito</h4>
