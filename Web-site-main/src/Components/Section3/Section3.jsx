10fdae58412a5cd0af234fe6f7af7ccfb773cbc5
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-06-22 16:38:12 -0300

    add section html

--- Web-site-main/src/Components/Section3/Section3.jsx
+++ Web-site-main/src/Components/Section3/Section3.jsx
@@ -3,38 +3,40 @@ import img1 from "./img1.png"
 
 function Section3(){
     return<>
-        <div className="bg_section3">
-            <div className="row">
-                
-                <div className="col-md-4">
-                    <img className= "img-1" src={img1} alt=""/>
+        <section>
+            <div className="bg_section3">
+                <div className="row">
+                    
+                    <div className="col-md-4">
+                        <img className= "img-1" src={img1} alt=""/>
 
-                    <p className="name">Rafael Silva</p>
-                    <p className="text">Comprei o plano Empresarial. Gostei muito
-                    minha empresa cresceu demais foi incrivel a criação de sites
-                    feita pelos funcionários. 
-                    </p>
-                </div>
+                        <p className="name">Rafael Silva</p>
+                        <p className="text">Comprei o plano Empresarial. Gostei muito
+                        minha empresa cresceu demais foi incrivel a criação de sites
+                        feita pelos funcionários. 
+                        </p>
+                    </div>
 
-                <div className="col-md-4">
-                    <img className= "img-2" src={img1} alt=""/>
+                    <div className="col-md-4">
+                        <img className= "img-2" src={img1} alt=""/>
 
-                    <p className="name">Carla Sales</p>
-                    <p className="text">Comecei com o plano Gratuito e consegui desenvolver meu primeiro site
-                    depois assinei o plano Analista, o pessoal é bem bacana aprendi muito e fiz vários projetos.
-                    </p>
-                </div>
+                        <p className="name">Carla Sales</p>
+                        <p className="text">Comecei com o plano Gratuito e consegui desenvolver meu primeiro site
+                        depois assinei o plano Analista, o pessoal é bem bacana aprendi muito e fiz vários projetos.
+                        </p>
+                    </div>
 
-                <div className="col-md-4">
-                    <img className= "img-3" src={img1} alt=""/>
+                    <div className="col-md-4">
+                        <img className= "img-3" src={img1} alt=""/>
 
-                    <p className="name">Rodrigo Raia</p>
-                    <p className="text">gostei muito do plano Gratuito, em breve vou assinar o plano Analista 
-                    vale muito o a pena! o pessoal é bem bacana quando ensina na plataforma de cursos.
-                    </p>
+                        <p className="name">Rodrigo Raia</p>
+                        <p className="text">gostei muito do plano Gratuito, em breve vou assinar o plano Analista 
+                        vale muito o a pena! o pessoal é bem bacana quando ensina na plataforma de cursos.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </>
 }
 
--- Web-site-main/src/Components/Section4/about.jsx
+++ Web-site-main/src/Components/Section4/about.jsx
@@ -1,18 +1,20 @@
 import React from "react"
 export default function About(){
-    return<>    
-        <div className="about-container" id="about">
-            <div className="about-conteudo">
-                <h2 className="title-about">O que é a UYI</h2>
-                <p className="About-text">A empresa fictícia UYI tem o objetivo de prestart
-                serviços em diversas áreas da Tecnologia da Informação, uma delas é 
-                a construção de sites para négocios e e-commerce online, também temos 
-                Analistas especializados em Buiness Analsys, cujo objetivo é 
-                alavancar empresas que precisam de um negócio on-line, a empresa surgiu
-                a partir de uma start-up em 2019 após a criação de um site experimental
-                onde o tal modelo exibia modelos 3d em tecnologia three.js o mesmo
-                serviu de inspiração.</p>
+    return<>
+        <section>    
+            <div className="about-container" id="about">
+                <div className="about-conteudo">
+                    <h2 className="title-about">O que é a UYI</h2>
+                    <p className="About-text">A empresa fictícia UYI tem o objetivo de prestart
+                    serviços em diversas áreas da Tecnologia da Informação, uma delas é 
+                    a construção de sites para négocios e e-commerce online, também temos 
+                    Analistas especializados em Buiness Analsys, cujo objetivo é 
+                    alavancar empresas que precisam de um negócio on-line, a empresa surgiu
+                    a partir de uma start-up em 2019 após a criação de um site experimental
+                    onde o tal modelo exibia modelos 3d em tecnologia three.js o mesmo
+                    serviu de inspiração.</p>
+                </div>
             </div>
-        </div>
+        </section>
     </>
 }
