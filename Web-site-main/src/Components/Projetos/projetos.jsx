ab2fc94a361f10f045be3891e02351c0a86168cb
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-06-22 09:50:55 -0300

    metodologia (BEM) aplicado no html

--- Web-site-main/src/Components/Projetos/projetos.jsx
+++ Web-site-main/src/Components/Projetos/projetos.jsx
@@ -7,49 +7,41 @@ import img4 from "./img4.jpg"
 
 export default function Projetos(){
     return<>
-    <div className="project-bg">
-        <div className="project__card">
-            <div className="tech__card">
-                <div className="card">
+        <section className="project">
+            <div className="project__card">
+                <div className="project__content">
                     <img src={img1} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Tecnologias que Utilizadmos:</h5>
-                        <p className="card-text">Utilizamos css,Javascript,html e alguns frameworks como: boostrap.</p>
+                    <div className="project__body">
+                        <h5 className="project__title">Tecnologias que Utilizadmos:</h5>
+                        <p className="project__text">Utilizamos css,Javascript,html e alguns frameworks como: boostrap.</p>
                         <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
                 </div>
-            </div>
-            <div className="tech__card">
-                <div className="card">
+                <div className="project__content">
                     <img src={img2} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Nossos Programadores</h5>
-                        <p className="card-text">Nossas programadores são especialistas em Javascript, css e backend para banco de dados.</p>
+                    <div className="project__body">
+                        <h5 className="project__title">Nossos Programadores</h5>
+                        <p className="project__text">Nossas programadores são especialistas em Javascript, css e backend para banco de dados.</p>
                         <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
                 </div>
-            </div>
-            <div className="tech__card">
-                <div className="card">
+                <div className="project__content">
                     <img src={img3} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Designers</h5>
-                        <p className="card-text">São responsáveis pelo nosso front-end executam um Ui excelente para negócios.</p>
+                    <div className="project__body">
+                        <h5 className="project__title">Designers</h5>
+                        <p className="project__text">São responsáveis pelo nosso front-end executam um Ui excelente para negócios.</p>
                         <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba mais</a>
                     </div>
                 </div>
-            </div>
-            <div className="col col-container">
-                <div className="card">
+                <div className="project__content">
                     <img src={img4} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Analistas</h5>
-                        <p className="card-text">Toda nossa base de negócios, com o Buiness Analsys conseguimos um marketing objetivo.</p>
+                    <div className="project__body">
+                        <h5 className="project__title">Analistas</h5>
+                        <p className="project__text">Toda nossa base de negócios, com o Buiness Analsys conseguimos um marketing objetivo.</p>
                         <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </section>
     </>
 }
