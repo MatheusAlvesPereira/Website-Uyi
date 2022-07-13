03551dd7912d5d422f1ac5c24768bb2972be032c
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-07-13 19:25:55 -0300

    remove boostrap banner.jsx

--- Web-site-main/src/Components/Banner/Banner.jsx
+++ Web-site-main/src/Components/Banner/Banner.jsx
@@ -1,14 +1,12 @@
 import React from "react"
-import img from "./img.jpg"
 import "./banner.css"
 
 function Banner(){
     return<>
         <section className="banner">
+            <div className="img-banner"></div>
             <div className="wrapper">
-                <img src={img} className="img_banner" alt=""/>
-
-                <label id="texto_h1">Escolha o Seu Destino.... <br/>O Visionário, A Technologia.</label>
+                <label className="title">Escolha o Seu Destino.... <br/>O Visionário, A Technologia.</label>
                 <p className="text_banner">Somos A UYi empresa de Tecnologia que presta serviços em diversas áreas em Ti.
                 Fazemos o seu site, fornecemos ferramentas para criação de vídeos, banners, flayers e diversos meios de
                 comunicação. Business Analysis Inteligence para Sites de grande porte, ou Micro empresas .
