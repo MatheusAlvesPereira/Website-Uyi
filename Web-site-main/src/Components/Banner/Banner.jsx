44756e8604504490cadb11616653fe065e91218c
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-07-18 18:28:23 -0300

    path fill arrow

--- Web-site-main/src/Components/Banner/Banner.jsx
+++ Web-site-main/src/Components/Banner/Banner.jsx
@@ -13,7 +13,7 @@ function Banner(){
                 </p>
             </div>
 
-            <svg width="95" height="101" viewBox="0 0 95 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="banner__arrow" width="95" height="101" viewBox="0 0 95 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M57.3535 81.5261L50.4925 88.8205V0H44.5077V88.8205L37.6465 81.5261L33.4147 86.025L47.5001 101L61.5853 86.025L57.3535 81.5261Z" fill="black"/>
             </svg>
         </section>
