3a6aeca5f9f66ab3ce838d404ed0ef2d1504ccae
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-07-22 07:45:02 -0300

    card responsive

--- Web-site-main/src/Components/Projetos/projetos.jsx
+++ Web-site-main/src/Components/Projetos/projetos.jsx
@@ -10,36 +10,32 @@ export default function Projetos(){
         <section className="project">
             <div className="project__card">
                 <div className="project__content">
-                    <img src={img1} className="card-img-top" alt="..."/>
                     <div className="project__body">
                         <h5 className="project__title">Tecnologias que Utilizadmos:</h5>
                         <p className="project__text">Utilizamos css,Javascript,html e alguns frameworks como: boostrap.</p>
-                        <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
+                    <img src={img1} className="card-img-top" alt="..."/>
                 </div>
                 <div className="project__content">
-                    <img src={img2} className="card-img-top" alt="..."/>
                     <div className="project__body">
                         <h5 className="project__title">Nossos Programadores</h5>
                         <p className="project__text">Nossas programadores são especialistas em Javascript, css e backend para banco de dados.</p>
-                        <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
+                    <img src={img2} className="card-img-top" alt="..."/>
                 </div>
                 <div className="project__content">
-                    <img src={img3} className="card-img-top" alt="..."/>
                     <div className="project__body">
                         <h5 className="project__title">Designers</h5>
                         <p className="project__text">São responsáveis pelo nosso front-end executam um Ui excelente para negócios.</p>
-                        <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba mais</a>
                     </div>
+                    <img src={img3} className="card-img-top" alt="..."/>
                 </div>
                 <div className="project__content">
-                    <img src={img4} className="card-img-top" alt="..."/>
                     <div className="project__body">
                         <h5 className="project__title">Analistas</h5>
                         <p className="project__text">Toda nossa base de negócios, com o Buiness Analsys conseguimos um marketing objetivo.</p>
-                        <a href="#" className="btn btn-primary" role="button" data-bs-toggle="button">Saiba Mais</a>
                     </div>
+                    <img src={img4} className="card-img-top" alt="..."/>
                 </div>
             </div>
         </section>
