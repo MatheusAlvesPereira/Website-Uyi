010b41cff1860370867b08821e060672c537de16
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-08-12 18:35:13 -0300

    remove nav.js

--- Web-site-main/src/Components/Nav/nav.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu');
-const nav = document.querySelector('nav');
-const close = document.querySelector('.close')
-
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav')
-})
