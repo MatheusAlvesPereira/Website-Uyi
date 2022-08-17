7037eafc9f9ca3888398e59d86b8aad748a5d1dc
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-08-17 20:20:15 -0300

    navbar js

---
+++ Web-site-main/src/Components/Nav/nav.js
@@ -0,0 +1,6 @@
+
+export default function navbar( () => {
+    const menu = document.querySelector('.menu');
+    const close = document.querySelector('.close');
+    const nav = document.querySelector('.nav');
+)};
