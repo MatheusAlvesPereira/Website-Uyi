9a0d6ea49925dca640108a2fd4282b9afb283c3e
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-08-29 08:40:32 -0300

    bug price

--- Web-site-main/src/site.jsx
+++ Web-site-main/src/site.jsx
@@ -5,7 +5,6 @@ import Banner from './Components/Banner/Banner'
 import Technology from './Components/technology/technology'
 import Card from './Components/Card/card'
 import Coments from './Components/Coments/Coments'
-import Carousel from "./Components/Carousel/carousel"
 import Projetos from "./Components/Projetos/projetos"
 import Footer from "./Components/footer/footer"
 import About from "./Components/about/about"
@@ -15,7 +14,6 @@ export default function App(){
     <Nav/>
     <Banner/>
     <Projetos/>
-    <Carousel/>
     <Technology/>
     <Card/>
     <Coments/>
