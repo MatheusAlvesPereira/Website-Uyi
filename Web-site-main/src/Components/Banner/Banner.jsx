fb0135f49aa90bcdb95874e28b955f84d9d5af08
Matheus Alves <matheusalvessilvabr@gmail.com>
2022-09-14 17:49:34 -0300

    figma space title and text fixing

--- Web-site-main/src/Components/Banner/Banner.jsx
+++ Web-site-main/src/Components/Banner/Banner.jsx
@@ -5,16 +5,12 @@ export default function Banner(){
         <section className="banner">
             <div className="banner__img"></div>
             <div className="banner__wrapper">
-                <label className="banner__title">Tecnology in<br/>your Buiness</label>
+                <label className="banner__title">Tecnology in<br/>your buiness</label>
                 <p className="banner__text">Employers designer<br/>
                     buiness analysys 
                     back end support.
                 </p>
-            </div>
-
-            <svg className="banner__arrow" width="95" height="101" viewBox="0 0 95 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M57.3535 81.5261L50.4925 88.8205V0H44.5077V88.8205L37.6465 81.5261L33.4147 86.025L47.5001 101L61.5853 86.025L57.3535 81.5261Z" fill="black"/>
-            </svg>  
+            </div> 
         </section>
     </>
 }
