8d674d905a8d170fb75880e87f2e67c2207fa7d1
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-03-23 16:06:21 -0300

    profile page version

--- Web-site-main/src/Components/Projetos/projetos.jsx
+++ Web-site-main/src/Components/Projetos/projetos.jsx
@@ -4,7 +4,6 @@ import img2 from "./img2.jpg"
 import img3 from "./img3.jpg"
 import img4 from "./img4.jpg"
 
-
 export default function Projetos(){
     return<>
         <section className="project">
--- Web-site-main/src/config.php
+++ Web-site-main/src/config.php
@@ -1,17 +1,21 @@
 <?php
-
-    echo 'Hello world'
-
-    $username = '#';
-    $password = '#';
-    $dsn = '#';
+    $username = 'SYSDBA';
+    $password = '2302';
     $dbh = null;
 
     try {
-        $dbh = new PDO($dsn, $username, $password);
+        $pdo = new PDO("firebird:localhost=localhost;dbname=DATABASE",$username,$password);    
     } 
     catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
         exit;
     }
+
+    if(isset($_POST['email'])){
+        $sql = $pdo->prepare("INSERT INTO CLIENTES Values (null,?,?)");
+        $sql->execute(array($_POST['email'], $_POST['senha']));
+        echo('INSERT SUCESS!!!!');
+    }
+
+    echo('funcionou');  
 ?>
